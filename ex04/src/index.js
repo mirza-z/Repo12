2445d449b04c77e1933593928a4c703bfaf2fa17
mirza-z <zaimovic330@gmail.com>
2022-04-09 16:18:55 +0200

    Updated the code in this one

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -2,6 +2,7 @@
 // Only change code below this line
 function sumFibonacci(num){
     if(num==1) return 1;
+    if(num<1) return 0;
     var sum = 0, fib = [1 , 1];
     for(let i = 2; i <= num; i++){
         if(fib[i-1]+fib[i-2] > num){
@@ -18,5 +19,5 @@ function sumFibonacci(num){
 }
 // Only change code above this line
 
-console.log(sumFibonacci(20)); // Change this line
+console.log(sumFibonacci(0)); // Change this line
 module.exports = sumFibonacci;
